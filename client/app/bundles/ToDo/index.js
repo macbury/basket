6a6b60ceb3829db13ae7ad0b730ed792b5a9554a
MacBury <macbury@gmail.com>
2016-10-18 14:44:11 +0200

    Moved redux store creation

--- client/app/bundles/ToDo/index.js
+++ client/app/bundles/ToDo/index.js
@@ -2,22 +2,18 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './reducers';
-import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 
-const reduxMiddlewares = applyMiddleware(
+import store from './store';
 
-);
+injectTapEventPlugin();
 
 window.onload = function() {
-  injectTapEventPlugin();
   ReactDOM.render(
     <MuiThemeProvider>
-      <Provider store={ compose(reduxMiddlewares)(createStore)(store) }>
+      <Provider store={ store }>
         <RaisedButton label="Hello!" />
       </Provider>
     </MuiThemeProvider>,
--- client/app/bundles/ToDo/reducers/index.js
+++ client/app/bundles/ToDo/reducers/index.js
@@ -1,6 +1,18 @@
 import { combineReducers } from 'redux';
 import reducerLists from './reducer_lists';
 
-export default combineReducers({
+/**
+* Main app reducer
+*/
+const reducer = combineReducers({
   lists: reducerLists
 });
+
+/**
+* Initial todo app state
+*/
+const initialState = {
+
+};
+
+export { initialState, reducer };
